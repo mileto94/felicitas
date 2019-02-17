b0142e96b27f968d1802ce5df29708153ebfa6e0
Milka Ferezliyska <mferezliyska@melontech.com>
2019-02-17 23:10:56 +0200

    Felicitas: Add S3 bucket as media storage.

--- felicitas/felicitas/settings.py
+++ felicitas/felicitas/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'django_extensions',
     'corsheaders',
+    'storages',
 
     # custom
     'game_rules',
@@ -152,6 +153,20 @@ SNS_SETTINGS = {
         'TopicArn': creds.SNS_SETTINGS['updateGamePolls']['TopicArn'] or ''
     }
 }
+
+AWS_STORAGE_BUCKET_NAME = creds.AWS_STORAGE_BUCKET_NAME or ''
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+# AWS_LOCATION = 'static'
+#
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'felicitas/static'),
+# ]
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'felicitas.storage_backends.MediaStorage'
 
 # ----------- AWS Settings -------------
 
