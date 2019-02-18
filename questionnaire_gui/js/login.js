230d6a126e560696e00d4d6e1ed4c331fa735621
Milka Ferezliyska <mferezliyska@melontech.com>
2019-02-18 22:03:03 +0200

    GUI: Update login process in order to verify user token to QuizGame.

--- questionnaire_gui/js/login.js
+++ questionnaire_gui/js/login.js
@@ -3,20 +3,34 @@
 
     $('#signin').submit(function (event) {
         event.preventDefault();
-
+        var username = $('#username').val();
         $.ajax({
             url: 'http://localhost:8002/rest-auth/login/',
             method: 'POST',
             data: {
-                'username': $('#username').val(),
+                'username': username,
                 'password': $('#password').val(),
             },
         }).done(function (data) {
             localStorage.setItem('user_key', data.key);
+            $.ajax({
+                url: 'http://localhost:8001/validate-token/',
+                method: 'POST',
+                data: {
+                    'username': username,
+                    'token': data.key,
+                },
+            }).done(function (data) {
+               alert('You have successfully logged in.');
+               localStorage.setItem('user_id', data.user_id);
+            }).fail(function (data) {
+               alert('Sorry, There is an error in the registration process. Please, try again.');
+               console.log(data);
+            });
            alert('You have successfully logged in.');
         }).fail(function (data) {
            alert('Sorry, There is an error in the registration process.');
-           console.log(data)
+           console.log(data);
         });
     });
 
