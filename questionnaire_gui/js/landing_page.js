95d4cf9f8f2cc56fd553ec8b10c2ede7ff552dbc
Milka Ferezliyska <mferezliyska@melontech.com>
2019-02-21 20:48:02 +0200

    GUI: Update user controls according to user credentials in the localStorage.

--- questionnaire_gui/js/landing_page.js
+++ questionnaire_gui/js/landing_page.js
@@ -63,30 +63,3 @@ function startGame(game_type_id, game_name, game_polls_count) {
     localStorage.setItem('polls_counter', 0);
     window.location.href='play_game.html';
 }
-
-function logOut() {
-    $.ajax({
-        url: 'http://localhost:8002/rest-auth/logout/',
-        method: 'POST',
-        data: {},
-    }).done(function (data) {
-        $.ajax({
-            url: 'http://localhost:8001/log-out/',
-            method: 'POST',
-            data: {
-                'user_id': localStorage.getItem('user_id'),
-                'token': localStorage.getItem('user_key'),
-            },
-        }).done(function (data) {
-           alert('You have successfully logged out.');
-           localStorage.clear();
-        }).fail(function (data) {
-           alert('Sorry, There is an error in the log out process. Please, try again.');
-           console.log(data);
-        });
-       alert('You have successfully logged out.');
-    }).fail(function (data) {
-       alert('Sorry, There is an error in the log out process.');
-       console.log(data);
-    });
-}
---
+++ questionnaire_gui/js/main.js
@@ -0,0 +1,45 @@
+(function($) {
+    "use strict"; // Start of use strict
+    updateUserControls();
+})(jQuery);
+
+function logOut() {
+    $.ajax({
+        url: 'http://localhost:8002/rest-auth/logout/',
+        method: 'POST',
+        data: {},
+    }).done(function (data) {
+        $.ajax({
+            url: 'http://localhost:8001/log-out/',
+            method: 'POST',
+            data: {
+                'user_id': localStorage.getItem('user_id'),
+                'token': localStorage.getItem('user_key'),
+            },
+        }).done(function (data) {
+           alert('You have successfully logged out.');
+           localStorage.clear();
+           updateUserControls();
+        }).fail(function (data) {
+           alert('Sorry, There is an error in the log out process. Please, try again.');
+           console.log(data);
+        });
+       alert('You have successfully logged out.');
+    }).fail(function (data) {
+       alert('Sorry, There is an error in the log out process.');
+       console.log(data);
+    });
+}
+
+
+function updateUserControls() {
+    if(localStorage.getItem('user_key')) {
+        $('#log-out-btn').show();
+        $('#log-in-btn').hide();
+        $('#register-btn').hide();
+    } else {
+        $('#log-out-btn').hide();
+        $('#log-in-btn').show();
+        $('#register-btn').show();
+    }
+}
