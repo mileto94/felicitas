962905af95f23bba39f6a888491c2def552e57cb
Milka Ferezliyska <mferezliyska@melontech.com>
2019-02-23 11:16:43 +0200

    QuizGame: Add basic tests for user interactions.

--- quiz_game/quiz/tests.py
+++ quiz_game/quiz/tests.py
@@ -1,3 +1,123 @@
-from django.test import TestCase
+from unittest.mock import MagicMock, patch
 
-# Create your tests here.
+from django.forms.models import model_to_dict
+from django.test import TestCase
+from django.urls import reverse
+
+from quiz.models import Game, VoteLog
+
+
+class BaseQuizGameTestCase:
+    def create_game(self, player=1, game_type=1, result=0, finished=False, polls_list=[], answered_polls=[]):
+        return Game.objects.create(
+            player=player,
+            game_type=game_type,
+            result=result,
+            finished=finished,
+            polls_list=polls_list,
+            answered_polls=answered_polls
+        )
+
+    def create_vote_log(self, game, player=1, game_type=1, vote='', points=0, poll=1):
+        return VoteLog.objects.create(
+            player=player,
+            game_type=game_type,
+            game=game,
+            vote=vote,
+            points=points,
+            poll=poll
+        )
+
+
+class TestEndGame(BaseQuizGameTestCase, TestCase):
+    def setUp(self):
+        self.game = self.create_game()
+
+    def test_end_game(self):
+        url = reverse('end-game')
+        response = self.client.post(
+            url, data={'id': self.game.id}, content_type='application/json')
+        self.assertEqual(201, response.status_code)
+        self.game.finished = True
+        self.assertDictEqual(model_to_dict(self.game), response.json())
+
+
+class TestUserActions(TestCase):
+    def test_validate_token(self):
+        url = reverse('validate-token')
+        user_id = 1
+        user_response = MagicMock(status_code=200, POST={'user_id': user_id})
+        user_response.json = MagicMock(return_value={'user_id': user_id})
+
+        with patch('requests.post', return_value=user_response) as mock_post:
+            with patch('django.core.cache.cache.set') as mock_cache:
+                response = self.client.post(
+                    url,
+                    data={'token': 'token', 'username': 'username'},
+                )
+                self.assertEqual(200, response.status_code)
+                self.assertDictEqual({'user_id': user_id}, response.json())
+
+            self.assertTrue(mock_cache.called)
+            self.assertEqual(1, mock_cache.call_count)
+
+        self.assertTrue(mock_post.called)
+        self.assertEqual(1, mock_post.call_count)
+
+    def test_validate_token_bad_request(self):
+        url = reverse('validate-token')
+        user_id = 1
+        user_response = MagicMock(status_code=200, POST={'user_id': user_id})
+        user_response.json = MagicMock(return_value={'user_id': user_id})
+
+        with patch('requests.post', return_value=user_response) as mock_post:
+            with patch('django.core.cache.cache.set') as mock_cache:
+                response = self.client.post(
+                    url,
+                    data={'token': 'token', 'username': ''},
+                )
+                self.assertEqual(400, response.status_code)
+                self.assertDictEqual({'is_active': False}, response.json())
+
+            self.assertFalse(mock_cache.called)
+            self.assertEqual(0, mock_cache.call_count)
+
+        self.assertFalse(mock_post.called)
+        self.assertEqual(0, mock_post.call_count)
+
+    def test_log_out(self):
+        url = reverse('log-out')
+        user_id = 1
+        with patch('django.core.cache.cache.get', return_value=user_id) as mock_cache:
+
+            with patch('django.core.cache.cache.delete') as delete_cache:
+                response = self.client.post(
+                    url,
+                    data={'token': 'token', 'user_id': user_id},
+                )
+                self.assertEqual(200, response.status_code)
+                self.assertDictEqual({'status': 'OK'}, response.json())
+            self.assertTrue(delete_cache.called)
+            self.assertEqual(1, delete_cache.call_count)
+
+        self.assertTrue(mock_cache.called)
+        self.assertEqual(1, mock_cache.call_count)
+
+    def test_log_out_bad_request(self):
+        url = reverse('log-out')
+        user_id = 1
+        with patch('django.core.cache.cache.get', return_value=user_id) as mock_cache:
+
+            with patch('django.core.cache.cache.delete') as delete_cache:
+                response = self.client.post(
+                    url,
+                    data={'token': 'token', 'user_id': ''},
+                )
+                self.assertEqual(400, response.status_code)
+                self.assertDictEqual({'status': 'fail'}, response.json())
+
+            self.assertFalse(delete_cache.called)
+            self.assertEqual(0, delete_cache.call_count)
+
+        self.assertFalse(mock_cache.called)
+        self.assertEqual(0, mock_cache.call_count)
