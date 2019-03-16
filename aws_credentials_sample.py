85693bd54697261737dd6666b09862a0ac1d7c8c
Milka Ferezliyska <mferezliyska@melontech.com>
2019-03-16 10:52:01 +0200

    Add aws_credentials sample file

---
+++ aws_credentials_sample.py
@@ -0,0 +1,20 @@
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_REGION_NAME = ''
+AWS_STORAGE_BUCKET_NAME = ''
+
+
+SNS_SETTINGS = {
+    'collectGamePolls': {
+        'TopicArn': ''
+    }
+}
+
+# Microservices endpoints
+USER_MANAGER_URL = 'http://localhost:8002'
+GAME_SETUP_URL = 'http://localhost:8000'
+
+CACHE_SERVER_URL = '127.0.0.1'
+
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
